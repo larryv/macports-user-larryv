1194f934d77cb6727b4010620901c0f983d23964
Lawrence Velázquez <larryv@macports.org>
2013-02-04 21:25:44 +0000

    [sandbox] macports1.0: Merge from trunk.
    git-svn-id: https://svn.macports.org/repository/macports/users/larryv@102560 d073be05-634f-4543-b044-5fe20cf6d1d6

--- base/src/macports1.0/macports.tcl
+++ base/src/macports1.0/macports.tcl
@@ -1291,6 +1291,7 @@ proc macports::worker_init {workername portpath porturl portbuildpath options va
     $workername alias registry_active registry::active
     $workername alias registry_file_registered registry::file_registered
     $workername alias registry_port_registered registry::port_registered
+    $workername alias registry_list_depends registry::list_depends
 
     # deferred options processing.
     $workername alias getoption macports::getoption
@@ -1729,6 +1730,10 @@ proc _mportsearchpath {depregex search_path {executable 0} {return_match 0}} {
                 set found 1
                 break
             }
+        }
+
+        if {$found} {
+            break
         }
     }
     if {$return_match} {
